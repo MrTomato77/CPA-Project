f66203f1219de53b007451a9404a7ade2d3477bb
MrTomato77 <kreechaichana3@gmail.com>
2024-03-05 16:05:34 +0700

    add dialogflow messenger

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,13 @@ export default function Home() {
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
         <Hero />
+        <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+        <df-messenger
+          intent="WELCOME"
+          chat-title="ComputerPartsAssistants"
+          agent-id="cb9d67f5-716a-4bed-92cb-d06775ecd178"
+          language-code="th"
+        ></df-messenger>
       </div>
     </main>
   );
